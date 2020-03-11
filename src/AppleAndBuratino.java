71e7e32a8d29e0cf31b6445fd1102d86e029c5e8
DunaevaSvetlana <solnste1993@mail.ru>
2020-03-12 00:05:01 +0400

    add

---
+++ src/AppleAndBuratino.java
@@ -0,0 +1,12 @@
+public class AppleAndBuratino {
+    public static void main(String[] args) {
+        final int appleBuratino = 5;
+        final int otobralBarabasApple = 2;
+
+        final int ostalosAppleBuratino = appleBuratino - otobralBarabasApple;
+
+        System.out.println("appleBuratino = " + appleBuratino);
+        System.out.println("otobralBarabasApple = " + otobralBarabasApple);
+        System.out.println("ostalosAppleBuratino = " + ostalosAppleBuratino);
+    }
+}
